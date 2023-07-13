e09ce48c9aaf2a2f50446beb10e3a6bd6bb63242
rinshanibrahim <rinshanibrahim@gmail.com>
2023-07-13 21:09:03 +0530

    tenth

--- js/main.js
+++ js/main.js
@@ -166,6 +166,34 @@ $(document).ready(function() {
 
 
 
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
 
 
 
